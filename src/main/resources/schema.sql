c2ea40bc2d8c225a9d0ce90699543c52f200d71b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-10 21:38:39 -0300

    vehicles script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,15 @@ values(default, 'pedro', 'pedro@pedro.com', 'pedro', false);
 insert into user_table
 (id, name, email, password, is_admin)
 values(default, 'luiz', 'luiz@luiz.com', 'luiz', true);
+
+insert into vehicle_table
+(id, name, brand, model, image_path)
+values(default, 'onix', 'gm', 'sport', 'some-path');
+
+insert into vehicle_table
+(id, name, brand, model, image_path)
+values(default, '320i', 'bmw', 'sport', 'some-path');
+
+insert into vehicle_table
+(id, name, brand, model, image_path)
+values(default, 'renegade', 'jeep', 'sport', 'some-path');
