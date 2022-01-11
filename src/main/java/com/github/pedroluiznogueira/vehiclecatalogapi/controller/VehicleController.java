23ab7dc3092c52ef94f9e88b6927a42060592dac
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-11 01:05:01 -0300

    allowing cors

--- src/main/java/com/github/pedroluiznogueira/vehiclecatalogapi/controller/VehicleController.java
+++ src/main/java/com/github/pedroluiznogueira/vehiclecatalogapi/controller/VehicleController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/vehicles")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class VehicleController {
 
     private final UserRepository userRepository;
