66b710f54cdd2155adc281b250a075bc98529695
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-12 18:51:39 -0300

    production database configs

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,3 @@
-insert into user_table
-(id, name, email, password, is_admin)
-values(default, 'pedro', 'pedro@pedro.com', '$2a$10$UQ/jc34qDT6Xo4nhpHJyBuKNPhsrUvMJ4QNTTKcD4GCnKruXZ0QKK', false);
-insert into user_table
-(id, name, email, password, is_admin)
-values(default, 'admin', 'admin@admin.com', '$2a$10$R62huYbT6e7NYeyQcxLGju1LMHh2jmDyMdpoT2F.bmL..7vAqa6am', true);
-
 insert into vehicle_table
 (id, name, brand, model, image_path, price)
 values(default, 'onix', 'gm', 'sport', 'some-path', 2000);
